4b517498df359031df30f69d4796bf4fbb5e6011
archana-r3961 <archanar5105@gmail.com>
2025-11-18 22:53:10 +0530

    Add files via upload

---
+++ 1 Bit full Adder (1)/1 Bit full Adder/full_tb.v
@@ -0,0 +1,17 @@
+`timescale 10ns/10ps
+module full_tb;
+reg a,b,cin;
+wire sum,cout;
+full_adder ga(a,b,cin,sum,cout);
+initial begin
+a=0;b=0;cin=0;
+#10 a=0;b=1;cin=0;
+#10 a=0;b=0;cin=1;
+#10 a=1;b=0;cin=0;
+#10 a=1;b=0;cin=1;
+#10 a=1;b=1;cin=0;
+#10 a=1;b=1;cin=1;
+#40;
+end
+endmodule
+
